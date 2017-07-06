c968f2473b293b15237c219d13adc445200a1299
shahar <shahar@budgetao.com>
2017-07-06 18:24:47 +0300

    init

--- README.md
+++ README.md
@@ -14,5 +14,15 @@
 - Go to [https://<YOUR_SLACK_TEAM>.slack.com/apps/new/A0F7YS25R-bots]()
 - Enter a name for the bot to post with. (i.e. gcp-alert-service)
 - Click `Add bot integration`.
-- Wait until the UI displays the `API Token` and copy the string (i.e. xxxx-yyyyyyyyyyyy-zzzzzzzzzzzzzzzzzzzzzzzz)
-- Configure the deployment configuration using the [Datastore UI](https://console.cloud.google.com/datastore) by adding a Config {"name":"slackAPIToken","value":"<YOUR_SLACK_API_TOKEN>"} entity.
+- Wait until the UI displays the `API Token` and copy the string (i.e. xxxx-yyyyyyyyyyyy-zzzzzzzzzzzzzzzzzzzzzzzz).
+- Keep this token for using in the next step.
+
+## Configure
+- Open your browser at [AWS DynamoDB Console](https://console.aws.amazon.com/dynamodb/home)
+- Switch to the `Tables` tab.
+- Select the `Configs` table and go to the `Items` tab.
+- Edit the slackAPIToken and set the `value` to the slack API token created in the previous step.
+- Save the new value.
+- Select the `Tests` table and go to the `Items tab.
+- You can change or delete the existing tests.
+- You can also add new tests.
--- index.js
+++ index.js
@@ -116,15 +116,62 @@ function sendSlack(channel, message, apiToken) {
 }
 
 exports.initDynamoDB = function (event, context, callback) {
-    console.log('initDynamoDB');
-    /*    Promise.all([
-            writeDynamo("Configs", { name: "slackAPIToken", value: "" }),
-            writeDynamo("Tests", { name: "all", test: "true", message: "this was an event: ${JSON.stringify($)}", slackChannel: "aws" })
-        ])
-            .then(() => {
-                callback(null, {});
-            })
-            .catch(err => {
-                callback(err);
-            });*/
+    Promise.all([
+        writeDynamo("Configs", { name: "slackAPIToken", value: "" }),
+        writeDynamo("Tests", { name: "all", test: "true", message: "this was an event: ${JSON.stringify($)}", slackChannel: "aws" })
+    ])
+        .then(() => {
+            sendResponse(event, context, "SUCCESS", {});
+        })
+        .catch(err => {
+            sendResponse(event, context, "FAILED", err);
+        });
 };
+
+function sendResponse(event, context, responseStatus, responseData) {
+    var responseBody = JSON.stringify({
+        Status: responseStatus,
+        Reason: "See the details in CloudWatch Log Stream: " + context.logStreamName,
+        PhysicalResourceId: context.logStreamName,
+        StackId: event.StackId,
+        RequestId: event.RequestId,
+        LogicalResourceId: event.LogicalResourceId,
+        Data: responseData
+    });
+
+    console.log("RESPONSE BODY:\n", responseBody);
+
+    var https = require("https");
+    var url = require("url");
+
+    var parsedUrl = url.parse(event.ResponseURL);
+    var options = {
+        hostname: parsedUrl.hostname,
+        port: 443,
+        path: parsedUrl.path,
+        method: "PUT",
+        headers: {
+            "content-type": "",
+            "content-length": responseBody.length
+        }
+    };
+
+    console.log("SENDING RESPONSE...\n");
+
+    var request = https.request(options, function (response) {
+        console.log("STATUS: " + response.statusCode);
+        console.log("HEADERS: " + JSON.stringify(response.headers));
+        // Tell AWS Lambda that the function execution is done  
+        context.done();
+    });
+
+    request.on("error", function (error) {
+        console.log("sendResponse Error:" + error);
+        // Tell AWS Lambda that the function execution is done  
+        context.done();
+    });
+
+    // write data to request body
+    request.write(responseBody);
+    request.end();
+}
